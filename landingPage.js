0476a83026148d7316cbbf16504685e91a8df63a
Abel Haddis (Student) <ahaddis@sundevils.asu.edu>
2022-10-27 12:59:57 -0700

    Created sign_up.html
    I created the outline for my sign up form and a .js file which will link the form to my landing page. I now need to make a .css file to begin styling my sign up page.

---
+++ landingPage.js
@@ -0,0 +1,5 @@
+const sign_up = document.querySelectorAll(".action_button");
+
+sign_up.addEvemtListener("click", function(){
+    location.href = "www.yoursite.com" //Lead user to sign up form
+});
